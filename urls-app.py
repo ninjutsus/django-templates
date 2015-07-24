1f35e409dee9349b29b158b5b9439f09ad378294
wils0n <wiljm0.0@gmail.com>
2015-07-23 23:06:25 -0500

    add urls templates

---
+++ urls-app.py
@@ -0,0 +1,8 @@
+from django.conf.urls import include, url
+#from .views import NameView
+#from modules.appname import api
+
+urlpatterns = [
+	#url(r'^api/tags/$', TagList.as_view(), name='api-tag-list'),
+	#url(r'^some/url/$', NameView.as_view(), name='room-detail'),
+]
